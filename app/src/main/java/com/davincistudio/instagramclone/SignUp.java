59ccfc4239a1608a6bbcb5648cde97e1239209fd
Oscar Hernández <oscar@espaciodavinci.es>
2020-03-19 02:58:47 +0100

    SignUp and Login Parse

--- app/src/main/java/com/davincistudio/instagramclone/SignUp.java
+++ app/src/main/java/com/davincistudio/instagramclone/SignUp.java
@@ -1,5 +1,6 @@
 package com.davincistudio.instagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -102,7 +103,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     
             @Override
             public void onClick(View v) {
-            
+                Intent intent = new Intent(SignUp.this, SignUpLoginActivity.class);
+                startActivity(intent);
             }
         });
     }
